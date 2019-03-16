f76747532fe7522d410f8b3e3db61376cb7e6be2
Chris Lewis <colshag@gmail.com>
2019-03-16 10:23:35 -0600

    work better for linux and mac

--- Client/cosmica.sh
+++ Client/cosmica.sh
@@ -1,1 +1,17 @@
+echo Welcome to Cosmcia!
+echo ======================================
+echo I will now attempt to update your code 
+echo from the git repo play-cosmica on GitHub!
+python update.py
+echo ======================================
+echo Great! Now let's run the launcher!
+echo I'm leaving this command prompt active so
+echo that if the game crashes you can give a 
+echo screenshot of what went wrong. 
+echo ======================================
+echo Thanks for Playing!
+echo Chris/Jeremy/Steve
+echo NeuroJump. 2019.
+echo ======================================
 python launcher.py
+read -p "Press enter to exit"
